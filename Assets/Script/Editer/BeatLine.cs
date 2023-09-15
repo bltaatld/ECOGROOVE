42352fee866d52c8cbb4027069b17425c48b2215
Jun Choi <52606638+bltaatld@users.noreply.github.com>
2023-09-15 14:38:48 +0900

    line update

--- Assets/Script/Editer/BeatLine.cs
+++ Assets/Script/Editer/BeatLine.cs
@@ -8,6 +8,7 @@ using TMPro;
 public class BeatLine : MonoBehaviour
 {
     public GameObject line;
+    public GameObject anotherObject;
     public ScrollScript script;
     public float bpm;
     public static int offset = 0;
@@ -33,10 +34,24 @@ public class BeatLine : MonoBehaviour
             for (int i = 0; i < count; i++)
             {
                 float result = (ms * i) + offset;
-                GameObject @object = Instantiate(line, root.transform);
-                @object.transform.position = new Vector2(transform.position.y + (result * NoteWriter.enlargement * mul * Tap.canvasMul), 425f);
-                @object.transform.localScale = new Vector2(Tap.canvasMul, Tap.canvasMul);
-                @object.transform.GetChild(1).GetComponent<Text>().text = $"{result}ms";
+
+                // 4번째마다 오브젝트 소환
+                if (i % 4 == 0)
+                {
+                    GameObject @object = Instantiate(anotherObject, root.transform); // 다른 오브젝트 소환
+                                                                                     // 다른 오브젝트의 위치, 스케일, 텍스트 등 설정
+                    @object.transform.position = new Vector2(transform.position.y + (result * NoteWriter.enlargement * mul * Tap.canvasMul), 425f);
+                    @object.transform.localScale = new Vector2(Tap.canvasMul, Tap.canvasMul);
+                    @object.transform.GetChild(1).GetComponent<Text>().text = $"{result}ms";
+                }
+                else
+                {
+                    GameObject @object = Instantiate(line, root.transform); // 원래 오브젝트 소환
+                                                                            // 원래 오브젝트의 위치, 스케일, 텍스트 등 설정
+                    @object.transform.position = new Vector2(transform.position.y + (result * NoteWriter.enlargement * mul * Tap.canvasMul), 425f);
+                    @object.transform.localScale = new Vector2(Tap.canvasMul, Tap.canvasMul);
+                    @object.transform.GetChild(1).GetComponent<Text>().text = $"{result}ms";
+                }
             }
 
             root.transform.position = new Vector2(0, 0);
